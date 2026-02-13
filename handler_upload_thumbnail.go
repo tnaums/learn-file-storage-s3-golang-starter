becb51ad2017b817d68dad941b4c38081dbaf735
tnaums <tanaumann@outlook.com>
2026-02-13 16:33:55 -0600

    finished ch1l5, File Storage Multipart Uploads

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,13 +29,10 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	const maxMemory = 10 << 20
 	r.ParseMultipartForm(maxMemory)
-
-
 
 	// "thumbnail" should match the HTML form input name
 	file, header, err := r.FormFile("thumbnail")
@@ -43,20 +41,34 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 	defer file.Close()
+	the_string_i_want := header.Header.Get("content-type")
 
-	mediaType := r.headers.get("content-type")
+	//	mediaType := r.Header.Get("content-type")
 	b, _ := io.ReadAll(file)
 
 	videoData, err := cfg.db.GetVideo(videoID)
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "Unable to find video in database", err)
+		return
 	}
 
+	if userID != videoData.UserID {
+		respondWithError(w, http.StatusBadRequest, "You do not own that video", err)
+		return
+	}
+	
 	newThumbnail := thumbnail{
-		data: b,
-		mediaType: mediaType,
+		data:      b,
+		mediaType: the_string_i_want,
 	}
 	videoThumbnails[videoID] = newThumbnail
-	
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	newURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+	videoData.ThumbnailURL = &newURL
+	err = cfg.db.UpdateVideo(videoData)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to update video with new url", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, videoData)
 }
